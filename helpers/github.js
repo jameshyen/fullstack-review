c79a24812e74d9bed807b57ff6a59f3c54d88693
James Yen <jameshyen@gmail.com>
2018-02-14 10:55:07 -0800

    Must get tokens

--- helpers/github.js
+++ helpers/github.js
@@ -1,22 +1,12 @@
 const request = require('request');
-const config = require('../config.js');
+// const config = require('../config.js');
 
 let getReposByUsername = (username) => {
-  // TODO - Use the request module to request repos for a specific
-  // user from the github API
-
-  // The options object has been provided to help you out, 
-  // but you'll have to fill in the URL
-
-  request.get(`https://api.github.com/users/${username}/repos`);
-  /* let options = {
-    url: 'FILL ME IN',
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
-    }
-  }; */
-
+  request(`https://api.github.com/users/${username}/repos`, function (error, response, body) {
+    console.log(body);
+  });
 }
 
 module.exports.getReposByUsername = getReposByUsername;
+
+getReposByUsername('jameshyen');
