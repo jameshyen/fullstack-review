e14c344d829c06288d41dccb3e7d206cb945fd67
James Yen <jameshyen@gmail.com>
2018-02-14 13:34:13 -0800

    hit enter

--- react-client/src/components/Search.jsx
+++ react-client/src/components/Search.jsx
@@ -18,11 +18,17 @@ class Search extends React.Component {
     this.props.onSearch(this.state.term);
   }
 
+  enter(e) {
+    if (e.key === 'Enter') {
+      this.search();
+    }
+  }
+
   render() {
     return (
       <div>
         <h4>Add more repos!</h4>
-        Enter a GitHub username: <input value={this.state.terms} onChange={this.onChange.bind(this)} />       
+        Enter a GitHub username: <input value={this.state.terms} onChange={this.onChange.bind(this)} onKeyPress={this.enter.bind(this)} />       
         <button onClick={this.search.bind(this)}>Add Repos</button>
       </div>
     );
