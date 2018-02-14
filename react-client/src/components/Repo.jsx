a88f0ed57bc53566a70740a49aedcd0d4f2dc1c9
James Yen <jameshyen@gmail.com>
2018-02-14 12:17:16 -0800

    want numeric list

---
+++ react-client/src/components/Repo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Repo = ({ repo }) => (
+  <li>{repo.name}</li>
+);
+
+export default Repo;
--- react-client/src/components/RepoList.jsx
+++ react-client/src/components/RepoList.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Repo from './Repo.jsx';
 
-const RepoList = (props) => (
+const RepoList = ({ repos }) => (
   <div>
     <h4>Repo List Component</h4>
-    There are {props.repos.length} repos.
+    There are {repos.length} repos.
+    <ul>
+      {repos.map((repo) => {
+        return <Repo repo={repo} />;  
+      })}
+    </ul>
   </div>
 );
 
