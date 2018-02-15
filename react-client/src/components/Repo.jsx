02518fac2abd8b20b508f9ce121ef1fcbbe067aa
James Yen <jameshyen@gmail.com>
2018-02-14 16:00:48 -0800

    le new tab

--- react-client/src/components/Repo.jsx
+++ react-client/src/components/Repo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Repo = ({ repo }) => (
   <tr>
     <td>{repo.stars}</td>
-    <td><a href={repo.url}>{repo.name}</a></td>
+    <td><a href={repo.url} target="_blank">{repo.name}</a></td>
     <td>{repo.owner.username}</td>
   </tr>
 );
